950fb3441db2ca077f5bd3ca65f53183b9c77120
Samuel Langley <samuellangley@intelligencefusion.co.uk>
2023-02-17 21:53:10 +0000

    removed all references to password as this will now be handled in a separate service

--- src/cassandra.go
+++ src/cassandra.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/gocql/gocql"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 )
 
 var Session *gocql.Session
 
 func init() {
-	var err error
+	err := godotenv.Load()
+  	if err != nil {
+    log.Println("Couldn't find .env file. Using local environment variables instead.")
+  	}
 	cluster := gocql.NewCluster(os.Getenv("DB_HOST"))
 	cluster.Keyspace = os.Getenv("DB_KEYSPACE")
 	Session, err = cluster.CreateSession()
